a5b60048f030bf4ccd28b64342f6a0bf9e761462
Vladimir Slanina <vladimir.slanina.jr@gmail.com>
2026-06-28 12:02:36 +0200

    Add indexing for TriangleI

--- src/core/math/math.hpp
+++ src/core/math/math.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iterator>
+#include <vector>
 
 namespace math
 {
@@ -16,13 +17,34 @@ namespace math
     using Point2Df = Point2D<float>;
     using Point2Dd = Point2D<double>;
 
-
-
     struct TriangleI
     {
         size_t a;
         size_t b;
         size_t c;
+
+        void sort_indices()
+        {
+            if (a > b) std::swap(a, b);
+            if (b > c) std::swap(b, c);
+            if (a > b) std::swap(a, b);
+        }
+
+        size_t operator[](size_t index) const
+        {
+            switch (index)
+            {
+            case 0: return a;
+            case 1: return b;
+            case 2: return c;
+            default: throw std::out_of_range("TriangleI index out of range");
+            }
+        }
+    };
+
+    struct Polygon
+    {
+        std::vector<Point2Di> vertices;
     };
 
 }
