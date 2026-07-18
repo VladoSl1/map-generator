cb6371ccecbb7d9ae936960e7aae2b153ed2beda
Vladimir Slanina <vladimir.slanina.jr@gmail.com>
2026-07-18 10:44:43 +0200

    Add method for determining lenght of interval

--- src/core/math/interval.hpp
+++ src/core/math/interval.hpp
@@ -9,6 +9,7 @@ namespace math
     {
         T min, max;
 
+        constexpr T length() const { return max - min; }
         constexpr bool contains(T value) const { return value >= min && value <= max; }
         constexpr T clamp(T value) const { return std::clamp(value, min, max); }
     };
